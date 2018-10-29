285af6fa76575391ba833fdc60c0e1fb4f9a573e
Luke Brady <lbrad23105@gmail.com>
2018-10-29 16:36:55 -0400

    Made improvements to the index interface.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- src/postnote.py
+++ src/postnote.py
@@ -1,6 +1,7 @@
 import flask
 
-from flask import render_template, request, redirect, session
+from flask import render_template, redirect, session
+from flask_cors import cross_origin
 
 # Create the flask application object.
 app = flask.Flask(__name__)
@@ -11,14 +12,22 @@ def index():
     # Render the Postnote index template.
     return render_template('index.html')
 
+# Login to the Postnote service.
+@app.route('/login', methods=['GET'])
+def login():
+    if 'email' in session:
+        return redirect('/dashboard')
+    else:
+        return redirect('/')
+
 # Post message route.
-@app.route('/post', methods=['GET', 'POST'])
-def post():
+@app.route('/<user>/post', methods=['GET', 'POST'])
+@cross_origin()
+def post(user):
     if 'email' in session:
         return render_template('welcome')
-    return render_template('index')
+    return redirect('/')
 
 
 if __name__ == '__main__':
-    app.run(port = 8097, debug=True)
-
+    app.run(port=8097, debug=True)
