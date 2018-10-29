ba7c12f9119a9bcee9aad8a0ea019619ebfdf31d
Luke Brady <lbrad23105@gmail.com>
2018-10-29 15:04:06 -0400

    Working on templates and stylesheets.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- src/postnote.py
+++ src/postnote.py
@@ -1,6 +1,6 @@
 import flask
 
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 
 # Create the flask application object.
 app = flask.Flask(__name__)
@@ -9,8 +9,14 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     # Render the Postnote index template.
-    return render_template('index')
+    return render_template('index.html')
 
+# Post message route.
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    if 'email' in session:
+        return render_template('welcome')
+    return render_template('index')
 
 
 if __name__ == '__main__':
