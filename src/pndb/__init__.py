374a4afd31484d20d25b5669b7d4e51ad460ede2
Luke Brady <lbrad23105@gmail.com>
2018-11-06 19:54:00 -0500

    Working on some prepared statements for the db.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

---
+++ src/pndb/__init__.py
@@ -0,0 +1,59 @@
+import hashlib
+import mysql.connector as conn
+
+
+class PostnoteDB:
+    def __init__(self, host, user, passwd, database):
+        self.db = conn.connect(
+            host=host,
+            user=user,
+            passwd=passwd,
+            database=database
+        )
+
+    # Get the database connection object.
+    def get_db_conn(self):
+        print(self.db)
+        return self.db
+
+    def create_new_user(self, username, email, password, user_type = 1):
+        # Create the cursor that will be used to create the user.
+        cursor = self.db.cursor()
+        # Check to see if the password has been filled in, if so hash the given password.
+        if password != None:
+            passhash = hashlib.sha256(password).hexdigest()
+        else:
+            return 1
+        # Check to see if any of the fields do not match the database parameters.
+        if len(username) > 50 or len(email) > 256:
+        # Create the statement that will be used to create the new user.
+            sql = 'INSERT INTO User (Username, Email, Password, UserType)' \
+                  'VALUES ("{0}", "{1}", "{2}", "{3}")'.format(username, passhash, email, user_type)
+        # Execute the query to create the user.
+            try:
+                cursor.execute(sql)
+            except conn.Error:
+                return 1
+            cursor.close()
+        else:
+            return 1
+
+
+    # Return a users notes from the database.
+    def get_user_notes(self, username):
+        # Create the cursor from the database object.
+        cursor = self.db.cursor()
+        # Create the SQL statement that will run against the Postnote database.
+        sql = 'SELECT * FROM Notes WHERE Username = "{0}"'.format(username)
+        # Now try to find the notes in the db.
+        cursor.execute(sql)
+        # If the query was successful, fetch all results.
+        result = cursor.fetchall()
+        # Close the connection then return the results.
+        cursor.close()
+        return result
+
+
+if __name__ == '__main__':
+    pndb = PostnoteDB(host='192.168.33.40', user='root',passwd='rootpassword1234', database='Postnote')
+    pndb.get_db_conn()
--- src/postnote.py
+++ src/postnote.py
@@ -2,9 +2,11 @@ import flask
 
 from flask import render_template, redirect, session
 from flask_cors import cross_origin
+from pndb import PostnoteDB
 
 # Create the flask application object.
 app = flask.Flask(__name__)
+conn = PostnoteDB(host='192.168.33.40', user='root',passwd='rootpassword1234', database='Postnote')
 
 # Postnote site index.
 @app.route('/', methods=['GET'])
@@ -15,7 +17,7 @@ def index():
 # Login to the Postnote service.
 @app.route('/login', methods=['GET'])
 def login():
-    if 'email' in session:
+    if 'username' in session:
         return redirect('/dashboard')
     else:
         return render_template('login.html')
@@ -23,7 +25,7 @@ def login():
 # Signup for the Postnote service.
 @app.route('/signup', methods=['GET'])
 def signup():
-    if 'email' in session:
+    if 'username' in session:
         return redirect('/dashboard')
     else:
         return render_template('signup.html')
@@ -32,7 +34,7 @@ def signup():
 @app.route('/<user>/post', methods=['GET', 'POST'])
 @cross_origin()
 def post(user):
-    if 'email' in session:
+    if 'username' in session:
         return render_template('welcome')
     return redirect('/')
 
